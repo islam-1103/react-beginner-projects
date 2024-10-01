f85102cdb7ed01d8051c3cd25caf0c3df738164e
islam-1103 <mirlanovislam135@gmail.com>
2024-10-01 11:29:48 +0300

    new cr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./index.scss";
 
-const Modal = ({open, setOpen}) => {
+const Modal = ({ open, setOpen, children }) => {
+  return (
   <div className={`overlay animated ${open ? "show" : ""}`}>
     <div className="modal">
       <svg
@@ -13,21 +14,23 @@ const Modal = ({open, setOpen}) => {
         <title />
         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
       </svg>
+      {children}
     </div>
-  </div>;
+  </div>
+  )
 };
 
 function App() {
-  const[open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">
         ✨ Открыть окно
       </button>
-       <Modal open={open} setOpen={setOpen}>
-       <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         <h3>Это модельное окно</h3>
-        </Modal>
+      </Modal>
     </div>
   );
 }
