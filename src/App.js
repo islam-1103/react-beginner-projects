d360f06b543c1a6cfe78b6466d0ef004bdbf2728
islam-1103 <mirlanovislam135@gmail.com>
2024-10-01 11:13:23 +0300

    new

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const Modal = ({open, setOpen}) => {
         <title />
         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
       </svg>
-      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
     </div>
   </div>;
 };
@@ -25,9 +24,10 @@ function App() {
       <button onClick={() => setOpen(true)} className="open-modal-btn">
         ✨ Открыть окно
       </button>
-      {open && <Modal open={open} setOpen={setOpen}>
+       <Modal open={open} setOpen={setOpen}>
+       <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         <h3>Это модельное окно</h3>
-        </Modal>}
+        </Modal>
     </div>
   );
 }
