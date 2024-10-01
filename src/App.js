55955ef3afd7e9a634abd02d5d49f72d3be2fe59
islam-1103 <mirlanovislam135@gmail.com>
2024-10-01 10:31:00 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователя");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvute = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id === id))
+    } else
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users 
+      onChangeSearchValue={onChangeSearchValue}
+      searchValue={searchValue} 
+      items={users} 
+      isLoading={isLoading} />
       {/* <Success /> */}
     </div>
   );
